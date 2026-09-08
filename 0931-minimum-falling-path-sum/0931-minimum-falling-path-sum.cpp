4c82e9eb83d5550a1aa0906a3a1689b65e6c2349
ShivaniKumari91 <152966678+ShivaniKumari91@users.noreply.github.com>
2026-09-08 18:44:08 +0530

    Time: 7 ms (10.69%), Space: 15 MB (47.84%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,47 +1,34 @@
 class Solution {
 public:
-    int f(int i, int j, vector<vector<int>>& matrix,
-          vector<vector<int>>& dp) {
-
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        // Out of boundary
-        if (j < 0 || j > n-1)
-            return 1e9;
-
-        // Last row
-        if (i == m - 1)
-            return matrix[i][j];
-
-        // Already calculated
-        if (dp[i][j] != -101)
-            return dp[i][j];
-
-        int leftdiag = matrix[i][j] + f(i + 1, j - 1, matrix, dp);
-        int down = matrix[i][j] + f(i + 1, j, matrix, dp);
-        int rightdiag = matrix[i][j] + f(i + 1, j + 1, matrix, dp);
-
-        return dp[i][j] = min({leftdiag, down, rightdiag});
-    }
-
     int minFallingPathSum(vector<vector<int>>& matrix) {
-
         int m = matrix.size();
         int n = matrix[0].size();
-
         int ans = INT_MAX;
-
-        vector<vector<int>> dp(m, vector<int>(n, -101));
-
-        // Starting point can be anywhere in first row
-        for (int i = 0; i < n; i++) {
-
-            int mini = f(0, i, matrix, dp);
-
-            ans = min(ans, mini);
+        vector<vector<int>> dp(m,vector<int>(n));
+        for(int j = 0;j < n; j++){
+            dp[m-1][j] = matrix[m-1][j];
         }
 
+        for(int i = m-2;i >= 0;i--){
+            for(int j = 0; j< n; j++){
+                int leftdiag = 1e9;
+                int rightdiag = 1e9;
+
+                if(j-1 >= 0) leftdiag = matrix[i][j] + dp[i+1][j-1];
+
+                int down = matrix[i][j] + dp[i+1][j]; 
+
+                if(j+1 < n) rightdiag = matrix[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min({leftdiag,down,rightdiag});
+
+               
+            }
+        }
+        for(int j = 0; j<n; j++){
+            ans = min(ans,dp[0][j]);
+        }
         return ans;
+        
     }
-};
+}; 
