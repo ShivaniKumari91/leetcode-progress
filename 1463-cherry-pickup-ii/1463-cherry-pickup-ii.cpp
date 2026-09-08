0c9185b63f0885cc9ba304e22ee113f4fc7c8c9a
ShivaniKumari91 <152966678+ShivaniKumari91@users.noreply.github.com>
2026-09-09 01:46:00 +0530

    Time: 79 ms (13.81%), Space: 21 MB (30.8%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,61 +1,72 @@
 class Solution {
 public:
-    int solve(int i, int j1, int j2, vector<vector<int>>& grid,
-              vector<vector<vector<int>>>& dp) {
-
-        int n = grid.size();
-        int m = grid[0].size();
-
-        // Out of bounds
-        if (j1 < 0 || j1 >= m || j2 < 0 || j2 >= m)
-            return -1e9;
-
-        // Last row
-        if (i == n - 1) {
-            if (j1 == j2)
-                return grid[i][j1];
-
-            return grid[i][j1] + grid[i][j2];
-        }
-
-        // Already calculated
-        if (dp[i][j1][j2] != -1)
-            return dp[i][j1][j2];
-
-        int maxi = -1e9;
-
-        // 3 choices for Robot 1
-        for (int d1 = -1; d1 <= 1; d1++) {
-
-            // 3 choices for Robot 2
-            for (int d2 = -1; d2 <= 1; d2++) {
-
-                int value;
-
-                // Both robots on same cell
-                if (j1 == j2)
-                    value = grid[i][j1];
-                else
-                    value = grid[i][j1] + grid[i][j2];
-
-                value += solve(i + 1, j1 + d1, j2 + d2, grid, dp);
-
-                maxi = max(maxi, value);
-            }
-        }
-
-        return dp[i][j1][j2] = maxi;
-    }
-
     int cherryPickup(vector<vector<int>>& grid) {
 
         int n = grid.size();
         int m = grid[0].size();
 
         vector<vector<vector<int>>> dp(
-            n, vector<vector<int>>(m, vector<int>(m, -1))
+            n, vector<vector<int>>(m, vector<int>(m, 0))
         );
 
-        return solve(0, 0, m - 1, grid, dp);
+        // Base case: last row
+        for (int j1 = 0; j1 < m; j1++) {
+            for (int j2 = 0; j2 < m; j2++) {
+
+                if (j1 == j2)
+                    dp[n - 1][j1][j2] = grid[n - 1][j1];
+                else
+                    dp[n - 1][j1][j2] =
+                        grid[n - 1][j1] + grid[n - 1][j2];
+            }
+        }
+
+        // Move from bottom row to top row
+        for (int i = n - 2; i >= 0; i--) {
+
+            for (int j1 = 0; j1 < m; j1++) {
+
+                for (int j2 = 0; j2 < m; j2++) {
+
+                    int maxi = -1e9;
+
+                    // Robot 1: 3 directions
+                    for (int d1 = -1; d1 <= 1; d1++) {
+
+                        // Robot 2: 3 directions
+                        for (int d2 = -1; d2 <= 1; d2++) {
+
+                            int value;
+
+                            // Current row cherries
+                            if (j1 == j2)
+                                value = grid[i][j1];
+                            else
+                                value = grid[i][j1] + grid[i][j2];
+
+                            int nj1 = j1 + d1;
+                            int nj2 = j2 + d2;
+
+                            // Check boundaries
+                            if (nj1 >= 0 && nj1 < m &&
+                                nj2 >= 0 && nj2 < m) {
+
+                                value += dp[i + 1][nj1][nj2];
+
+                                maxi = max(maxi, value);
+                            }
+                        }
+                    }
+
+                    dp[i][j1][j2] = maxi;
+                }
+            }
+        }
+
+        // Initially:
+        // Robot 1 = column 0
+        // Robot 2 = column m-1
+
+        return dp[0][0][m - 1];
     }
 };
