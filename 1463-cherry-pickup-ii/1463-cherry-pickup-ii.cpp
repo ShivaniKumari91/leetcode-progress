fdb8ad0fb6aa0bc3a00de6d870963c824dbbc708
ShivaniKumari91 <152966678+ShivaniKumari91@users.noreply.github.com>
2026-09-09 11:02:19 +0530

    Time: 49 ms (52.05%), Space: 21.1 MB (30.8%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,64 +1,47 @@
 class Solution {
 public:
-    int cherryPickup(vector<vector<int>>& grid) {
+    int f(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>> &dp){
+        int m = grid.size();
+        int n = grid[0].size();
+        if(j1<0 ||j1>=n || j2<0 ||j2>=n) return -1e8;
 
-        int n = grid.size();
-        int m = grid[0].size();
+        if(dp[i][j1][j2]!=-1) return dp[i][j1][j2];
 
-        vector<vector<int>> prev(m, vector<int>(m, 0));
+        if(i == m-1){
+            if(j1==j2) return grid[i][j1];
 
-        // Last row = base case
-        for (int j1 = 0; j1 < m; j1++) {
-            for (int j2 = 0; j2 < m; j2++) {
-
-                if (j1 == j2)
-                    prev[j1][j2] = grid[n - 1][j1];
-                else
-                    prev[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+            else{
+                return grid[i][j1] + grid[i][j2];
             }
+            
         }
+        
+       
 
-        for (int i = n - 2; i >= 0; i--) {
+        int maxi = -1e8;
 
-            // Fresh DP table for current row
-            vector<vector<int>> temp(m, vector<int>(m, 0));
+        for(int dj1=-1; dj1<=1; dj1++){
+            for(int dj2=-1; dj2<=1;dj2++){
+                int value;
+                if(j1==j2){
+                    value = grid[i][j1] + f(i+1,dj1+j1,dj2+j2,grid,dp); 
 
-            for (int j1 = 0; j1 < m; j1++) {
-                for (int j2 = 0; j2 < m; j2++) {
-
-                    int maxi = -1e9;
-
-                    for (int d1 = -1; d1 <= 1; d1++) {
-                        for (int d2 = -1; d2 <= 1; d2++) {
-
-                            int nj1 = j1 + d1;
-                            int nj2 = j2 + d2;
-
-                            if (nj1 < 0 || nj1 >= m ||
-                                nj2 < 0 || nj2 >= m)
-                                continue;
-
-                            int value;
-
-                            if (j1 == j2)
-                                value = grid[i][j1];
-                            else
-                                value = grid[i][j1] + grid[i][j2];
-
-                            value += prev[nj1][nj2];
-
-                            maxi = max(maxi, value);
-                        }
-                    }
-
-                    temp[j1][j2] = maxi;
                 }
+                else{
+                    value = grid[i][j1] + grid[i][j2] + f(i+1,dj1+j1,dj2+j2,grid,dp);
+
+                }
+                maxi = max(maxi,value);
+
             }
-
-            // Current row becomes previous row
-            prev = temp;
         }
-
-        return prev[0][m - 1];
+        return dp[i][j1][j2] = maxi;
+    }
+    int cherryPickup(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<vector<int>>> dp(m,vector<vector<int>>(n,vector<int>(n,-1)));
+        return f(0,0,n-1,grid,dp);
+        
     }
 };
