88758ab2c7762afdd309d702719ec94722f95439
ShivaniKumari91 <152966678+ShivaniKumari91@users.noreply.github.com>
2026-09-13 03:29:55 +0530

    Time: 3 ms (86.01%), Space: 15.4 MB (55.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,66 +1,51 @@
 class Solution {
 public:
-
-    int f(int ind, int target, vector<int>& nums,
-          vector<vector<int>>& dp) {
-
-        // Base case
-        if(ind == 0) {
-
-            if(target == 0 && nums[0] == 0)
-                return 2;
-
-            if(target == 0 || target == nums[0])
-                return 1;
-
-            return 0;
-        }
-
-        if(dp[ind][target] != -1)
-            return dp[ind][target];
-
-        // Not take
-        int notTake = f(ind - 1, target, nums, dp);
-
-        // Take
-        int take = 0;
-
-        if(nums[ind] <= target)
-            take = f(ind - 1, target - nums[ind], nums, dp);
-
-        return dp[ind][target] = take + notTake;
-    }
-
-
     int findTargetSumWays(vector<int>& nums, int target) {
 
         int n = nums.size();
 
         int totalSum = 0;
-
         for(int x : nums)
             totalSum += x;
 
-
-        // S2 = (TS - target) / 2
-
-        // S2 cannot be negative
         if(totalSum - target < 0)
             return 0;
 
-        // (TS - target) must be even
         if((totalSum - target) % 2 != 0)
             return 0;
 
         int required = (totalSum - target) / 2;
 
-
-        // Count subsets having sum = required
         vector<vector<int>> dp(
             n,
-            vector<int>(required + 1, -1)
+            vector<int>(required + 1, 0)
         );
 
-        return f(n - 1, required, nums, dp);
+        // Base case
+        if(nums[0] == 0)
+            dp[0][0] = 2;
+        else
+            dp[0][0] = 1;
+
+        if(nums[0] != 0 && nums[0] <= required)
+            dp[0][nums[0]] = 1;
+
+        // Fill table
+        for(int ind = 1; ind < n; ind++) {
+
+            for(int sum = 0; sum <= required; sum++) {
+
+                int notTake = dp[ind - 1][sum];
+
+                int take = 0;
+
+                if(nums[ind] <= sum)
+                    take = dp[ind - 1][sum - nums[ind]];
+
+                dp[ind][sum] = take + notTake;
+            }
+        }
+
+        return dp[n - 1][required];
     }
 };
