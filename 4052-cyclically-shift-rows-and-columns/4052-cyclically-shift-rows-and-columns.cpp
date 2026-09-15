0a543a606b8b272946505c8400ed399117f2d6db
ShivaniKumari91 <152966678+ShivaniKumari91@users.noreply.github.com>
2026-09-16 01:16:18 +0530

    Time: 0 ms (100%), Space: 73 MB (99.84%) - LeetHub

--- 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.cpp
+++ 4052-cyclically-shift-rows-and-columns/4052-cyclically-shift-rows-and-columns.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> cyclicShift(int n, vector<vector<int>>& grid, vector<int>& rowShift, vector<int>& colShift) {
-         vector<vector<int>> ans(n, vector<int>(n));
+
+        vector<vector<int>> ans(n, vector<int>(n));
 
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < n; j++) {
 
-                // Step 1: shift row i to the left
+                // STEP 1: Row i ko LEFT shift karna hai.
+                // Left shift => column decrease hoga.
+                // Isliye: j - rowShift[i]
+                //
+                // +n => negative value avoid karne ke liye
+                // %n => answer ko 0 se n-1 ke range me lane ke liye
+
                 int newCol = (j - rowShift[i] + n) % n;
 
-                // Step 2: shift this column upward
+
+                // STEP 2: Ab element newCol par pahunch gaya.
+                // Isliye column shift ke liye
+                // colShift[newCol] use karenge,
+                // NOT colShift[j].
+                //
+                // Up shift => row decrease hoga.
+
                 int newRow = (i - colShift[newCol] + n) % n;
 
-                // Put element at its final position
+
+                // Element ki final position
                 ans[newRow][newCol] = grid[i][j];
             }
         }
